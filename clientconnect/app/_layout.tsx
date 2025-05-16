6388d8b7fb8fd5decf7ae030c5c6b18247e42527
Hirushi Rathnayake <hirushir99@gmail.com>
2025-05-16 18:52:30 +0530

    Create project structure

--- clientconnect/app/_layout.tsx
+++ clientconnect/app/_layout.tsx
@@ -1,5 +1,35 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="auto" />
+      
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#1D4ED8',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          contentStyle: {
+            backgroundColor: '#f9fafb',
+          },
+        }}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f9fafb',
+  },
+});
